8458957a6117100c0c0d5ada608fc36d4ec48f19
Flory Ann Evia <floryann.evia@gmail.com>
2020-12-02 14:05:37 -0600

    updated routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,58 +1,64 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-module.exports = app => {
-app.post("/api/notes", function (req, res) {
-        let newNote = req.body;
-        fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
-            if (err) throw err;
-            var notes = JSON.parse(data);
-            notes.push(newNote);
-            updateDb(notes);
-            return console.log("Added new note: " + newNote.title);
-        });
-        function updateDb(notes) {
-        fs.writeFile("./Develop/db/db.json", JSON.stringify(notes, '\t'), err => {
-            if (err) throw err;
-            return true;
-        });
-        }
-    });
+module.exports = (app) => {
+  app.post("/api/notes", function (req, res) {
+    let createdNote = req.body;
+    createdNote.id = uuidv4();
 
-app.get("/api/notes", function (req, res) {
-        fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
-            if (err) throw err;
-            var notes = JSON.parse(data);
-            res.json(notes);
-        });
-    });
-
-app.get("/api/notes/:id", function (req, res) {
-        fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
-            if (err) throw err;
-            var notes = JSON.parse(data);
-            res.json(notes);
-        });
-    });
-
-app.delete("/api/notes/:id", function (req, res) {
+    console.log(createdNote);
     fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
-        if (err) throw err;
-        var notes = JSON.parse(data);
-        res.json(notes);
+      if (err) throw err;
+      var noteTaker = JSON.parse(data);
+      noteTaker.push(createdNote);
+      updateDb(noteTaker);
+      res.redirect("/notes");
+      return console.log("Added new note: " + createdNote.title);
     });
-            notes.splice(data);
-            updateDb(notes);
-            console.log("Deleted note with id " + req.params.id);
-        });
+    function updateDb(noteTaker) {
+      fs.writeFile("./Develop/db/db.json", JSON.stringify(noteTaker, "\t"), (err) => {
+        if (err) throw err;
+        return true;
+      });
+    }
+  });
+  app.get("/api/notes", function (req, res) {
+    fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      var noteTaker = JSON.parse(data);
+      res.json(noteTaker);
+    });
+  });
+  app.get("/api/notes/:id", function (req, res) {
+    fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      var noteTaker = JSON.parse(data);
+      res.json(noteTaker);
+    });
 
-// routes
-app.get('/notes', function (req, res) {
-            res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+  app.delete("/api/notes/:id", function (req, res) {
+    console.log(req.params);
+    fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      var noteTaker = JSON.parse(data);
+      res.json(noteTaker);
+      noteTaker.splice(req.params.id);
+      if (!req.params.id) {
+        return res.status(400).json({
+          status: "error",
+          error: "req body cannot be empty",
         });
+      }
+    });
 
-app.get('*', function (req, res) {
-            res.sendFile(path.join(__dirname, "../public/index.html"));
-        });
-
+    console.log("Deleted note with id " + req.params.id);
+  });
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 };
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 
 require('./routes/routes')(app);
